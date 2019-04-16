03ba2e9a1b2f0967133172a484d94f5e148d94a6
skarmakar5 <skarmakar@salesforce.com>
2019-04-16 09:58:19 +0200

    s

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const jsforce= require('jsforce')
 const PORT = process.env.PORT || 5000
 
 express()
@@ -8,4 +7,4 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`)) 
